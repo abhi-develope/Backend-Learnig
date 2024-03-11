39f5b275b723e27d36471eca3356db7aee0a202d
abhi-develope <prajapatiabhishek320@gmail.com>
2024-03-11 22:10:47 +0530

    filter data

--- mongo_project/index.js
+++ mongo_project/index.js
@@ -8,15 +8,21 @@ mongoose.connect("mongodb://localhost:27017/demodb")
 
 const db = mongoose.connection  // start the connection with mongo db
 
-db.on("error", ()=>{
-    console.log("Error while connecting to db");
-});
-
 db.once("open",()=>{
     console.log("connected to MongoDB");
     // Logic to insert data into the db
     init()
+
+    // running queries
+
+    dbQueries();
+
 })
+
+db.on("error", ()=>{
+    console.log("Error while connecting to db");
+});
+
 
 async function init(){
     // logic to insert data in the db
@@ -31,4 +37,31 @@ async function init(){
     const std_obj = await studentModel.create(student)
 
     console.log(std_obj);
+}
+
+async function dbQueries(){
+    // Read the student data
+
+    // Read the student data based on id
+    try{
+        const student = await studentModel.findById("65eefe382ffd69e8c2faf8b4")
+        console.log(student);
+    }catch(err){
+        console.log(err);
+    }
+
+    // find data based on name
+    try{
+    const students = await studentModel.find({name : "Abhishek"})
+    console.log(students);
+
+}catch(err){
+    console.log(err);
+}
+
+// Deal with multiple conditions
+
+const stds = await studentModel.where("age").gt("10").where("name").equals("Abishek").limit(2)
+console.log(stds);
+
 }
