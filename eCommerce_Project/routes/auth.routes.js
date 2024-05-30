00b34c89152578045e7b489e972405b67605ed88
abhi-develope <prajapatiabhishek320@gmail.com>
2024-05-30 14:48:35 +0530

    add middle ware

--- eCommerce_Project/routes/auth.routes.js
+++ eCommerce_Project/routes/auth.routes.js
@@ -5,6 +5,7 @@
  */
 
 const authController = require("../controllers/auth.controller")
+const authMW = require("../middleware/auth.mw")
 
 module.exports = (app)=>{
     app.post("/ecomm/api/v1/auth/signup", authController.signup)
