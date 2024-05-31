defb759c0d0dbb2c94f729a522328fd09efb4226
abhi-develope <prajapatiabhishek320@gmail.com>
2024-05-31 17:28:13 +0530

    create category controller

---
+++ eCommerce_Project/controllers/category.controller.js
@@ -0,0 +1,39 @@
+const category_model = require("../model/category.model")
+
+
+/**
+ * controller for creating the category
+ * 
+ * POST localhost:8080/ecomm/api/v1/auth/categories
+ * 
+ * {
+ *   "name" : "household"
+ *   "description" : "This is the description"
+ * }
+ */
+
+exports.createNewCategory = async (req, res)=>{
+
+    //Read the req body
+
+    //Create the category object
+    const cat_data = {
+        name : req.body.name,
+        description : req.body.description
+    }
+
+    //Insert into mongodb
+    try {
+        const category = await category_model.create(cat_data)
+        return res.status(201).send(category)
+        
+    } catch (error) {
+        console.log(error);
+       return res.status(500).send({
+            message : "Error while creating the category"
+        })
+    }
+    
+
+    //return the response of the created category
+}
